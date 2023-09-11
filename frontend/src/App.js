11eac77fb0aa1444e9211e70fda4fb6411dba07c
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-09-11 15:54:55 +0200

    Setup login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 import Home from './containers/Home';
 import Login from './containers/Login';
@@ -8,21 +9,26 @@ import Activate from './containers/Activate';
 import ResetPassword from './containers/ResetPassword';
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Layout from './hocs/Layout';
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Routes>
-                <Route exact path='/' Component={Home} />
-                <Route exact path='/login' Component={Login} />
-                <Route exact path='/signup' Component={Signup} />
-                <Route exact path='/reset-password' Component={ResetPassword} />
-                <Route exact path='/password/reset/confirm/:uid/:token' Component={ResetPasswordConfirm} />
-                <Route exact path='/activate/:uid/:token' Component={Activate} />
-            </Routes>
-        </Layout>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route exact path='/' Component={Home} />
+                    <Route exact path='/login' Component={Login} />
+                    <Route exact path='/signup' Component={Signup} />
+                    <Route exact path='/reset-password' Component={ResetPassword} />
+                    <Route exact path='/password/reset/confirm/:uid/:token' Component={ResetPasswordConfirm} />
+                    <Route exact path='/activate/:uid/:token' Component={Activate} />
+                </Routes>
+            </Layout>
+        </Router>
+    </Provider>
 );
 
 export default App;
