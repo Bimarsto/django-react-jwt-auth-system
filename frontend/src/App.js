8118be55dfdf31cc654cf388c8954185ae82923f
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-09-11 11:09:20 +0200

    Setup frontend router

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+
+import Layout from './hocs/Layout';
+
+const App = () => (
+    <Router>
+        <Layout>
+            <Routes>
+                <Route exact path='/' Component={Home} />
+                <Route exact path='/login' Component={Login} />
+                <Route exact path='/signup' Component={Signup} />
+                <Route exact path='/reset-password' Component={ResetPassword} />
+                <Route exact path='/password/reset/confirm/:uid/:token' Component={ResetPasswordConfirm} />
+                <Route exact path='/activate/:uid/:token' Component={Activate} />
+            </Routes>
+        </Layout>
+    </Router>
+);
+
+export default App;
